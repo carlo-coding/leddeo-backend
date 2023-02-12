91459726e805937c36a36c2adc3f98d54aae5d26
carlo-coding <jaanc.rt@gmail.com>
2023-02-12 15:57:02 -0600

    added code for fonts management

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,4 @@
 #!/bin/bash
-LOCAL_FONT_DIR="/usr/share/fonts"
-# Define la lista de fuentes deseadas
-DESIRED_FONTS=(
-  "Open Sans"
-  "Roboto"
-  "Lato"
-  "Oswald"
-  "Slabo 27px"
-  "Montserrat"
-  "Raleway"
-  "Poppins"
-  "Nunito"
-  "Bakbak One"
-  "Comfortaa"
-  "Caveat"
-  "Comic Neue"
-  "Courier Prime"
-  "Lexend"
-  "Lobster"
-  "Lora"
-  "Merriweather"
-  "Spectral"
-  "Pacifico"
-  "Zeyada"
-  "Babylonica"
-  "Mukta"
-)
-# Recorre la lista de fuentes deseadas
-for font in "${DESIRED_FONTS[@]}"; do
-  # Construye la URL de descarga de la fuente
-  font_url="https://fonts.google.com/download?family=$(echo $font | tr ' ' '+')"
-  # Descarga la fuente
-  wget -P $LOCAL_FONT_DIR $font_url
-  # Descomprime el archivo zip
-  unzip -o -d $LOCAL_FONT_DIR "$LOCAL_FONT_DIR/$(basename $font_url)"
-  # Elimina el archivo zip
-  rm "$LOCAL_FONT_DIR/$(basename $font_url)"
-done
-
-echo "Las fuentes deseadas se descargaron correctamente"
-
 # Stop and remove all running containers
 if [ -z "$(docker ps -a -q)" ]; then
   echo "No hay contenedores que detener"
--- run.sh
+++ run.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
+LOCAL_FONT_DIR="/usr/share/fonts"
+# Define la lista de fuentes deseadas
+DESIRED_FONTS=(
+  "Open Sans"
+  "Roboto"
+  "Lato"
+  "Oswald"
+  "Slabo 27px"
+  "Montserrat"
+  "Raleway"
+  "Poppins"
+  "Nunito"
+  "Bakbak One"
+  "Comfortaa"
+  "Caveat"
+  "Comic Neue"
+  "Courier Prime"
+  "Lexend"
+  "Lobster"
+  "Lora"
+  "Merriweather"
+  "Spectral"
+  "Pacifico"
+  "Zeyada"
+  "Babylonica"
+  "Mukta"
+)
+# Recorre la lista de fuentes deseadas
+for font in "${DESIRED_FONTS[@]}"; do
+  # Construye la URL de descarga de la fuente
+  font_url="https://fonts.google.com/download?family=$(echo $font | tr ' ' '+')"
+  # Descarga la fuente
+  wget -P $LOCAL_FONT_DIR $font_url
+  # Descomprime el archivo zip
+  unzip -o -d $LOCAL_FONT_DIR "$LOCAL_FONT_DIR/$(basename $font_url)"
+  # Elimina el archivo zip
+  rm "$LOCAL_FONT_DIR/$(basename $font_url)"
+done
 
+# Run django commands to start server
 python manage.py makemigrations
 python manage.py migrate
 python manage.py shell -c "from django.contrib.auth import get_user_model; print(get_user_model().objects.filter(is_superuser=True).exists())" | grep -q "True"
