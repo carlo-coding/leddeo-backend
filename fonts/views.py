a0e94aacc15ba6804d2acb86a34ba4ef6bc3e692
carlo-coding <jaanc.rt@gmail.com>
2023-02-13 13:04:54 -0600

    adjustment in LOCAL_FONT_DIR

--- fonts/views.py
+++ fonts/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 import os
 
 #C:\Windows\Fonts
-LOCAL_FONT_DIR = "C:\Windows\Fonts"
+LOCAL_FONT_DIR = "/usr/share/fonts"
 
 class ListFontsView(APIView):
   def get(self, request):
